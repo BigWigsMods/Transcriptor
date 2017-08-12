2aa54f6c8e9b44da570d3323bf6dc9d40d02a7fe
funkydude <funkydude@users.noreply.github.com>
2017-08-12 02:37:23 +0100

    SpecialEvents: Add Fallen Avatar.

--- SpecialEvents.lua
+++ SpecialEvents.lua
@@ -1,7 +1,7 @@
 local n, tbl = ...
 tbl.specialEvents = {
-	-- Tomb of Sargeras
 	["UNIT_SPELLCAST_SUCCEEDED"] = {
+		--[[ Tomb of Sargeras ]]--
 		[235268] = { -- Lunar Ghost
 			[118523] = "Stage 2", -- Huntress Kasparian (Sisters of the Moon)
 			[118374] = "Stage 3", -- Captain Yathae Moonstrike (Sisters of the Moon)
@@ -11,16 +11,25 @@ tbl.specialEvents = {
 		},
 	},
 	["SPELL_AURA_APPLIED"] = {
+		--[[ Tomb of Sargeras ]]--
 		[244834] = { -- Nether Gale
 			[117269] = "Intermission 1", -- Kil'jaeden
 		},
 	},
 	["SPELL_AURA_REMOVED"] = {
+		--[[ Tomb of Sargeras ]]--
+		[234891] = { -- Wrath of the Creators
+			[118289] = "Wrath Over", -- Maiden of Vigilance
+		},
 		[244834] = { -- Nether Gale
 			[117269] = "Stage 2", -- Kil'jaeden
 		},
+		[241983] = { -- Deceiver's Veil
+			[117269] = "Stage 3", -- Kil'jaeden
+		},
 	},
 	["SPELL_CAST_START"] = {
+		--[[ Tomb of Sargeras ]]--
 		[232174] = { -- Frosty Discharge
 			[116407] = "Discharge", -- Harjatan the Bludger
 		},
@@ -28,12 +37,10 @@ tbl.specialEvents = {
 			[117269] = "Intermission 2", -- Kil'jaeden
 		},
 	},
-	["SPELL_AURA_REMOVED"] = {
-		[234891] = { -- Wrath of the Creators
-			[118289] = "Wrath Over", -- Maiden of Vigilance
-		},
-		[241983] = { -- Deceiver's Veil
-			[117269] = "Stage 3", -- Kil'jaeden
+	["SPELL_CAST_SUCCESS"] = {
+		--[[ Tomb of Sargeras ]]--
+		[235597] = { -- Annihilation
+			[116939] = "Stage 2", -- Fallen Avatar
 		},
 	},
 }
