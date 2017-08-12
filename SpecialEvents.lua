b8c957b0686bb51a4918df3a9a44fea44259870d
funkydude <funkydude@users.noreply.github.com>
2017-08-12 02:04:26 +0100

    SpecialEvents: Add Maiden of Vigilance.

--- SpecialEvents.lua
+++ SpecialEvents.lua
@@ -3,20 +3,31 @@ tbl.specialEvents = {
 	-- Tomb of Sargeras
 	["UNIT_SPELLCAST_SUCCEEDED"] = {
 		[235268] = { -- Lunar Ghost
-			[118523] = "Stage 2", -- Huntress Kasparian
-			[118374] = "Stage 3", -- Captain Yathae Moonstrike
-		}
+			[118523] = "Stage 2", -- Huntress Kasparian (Sisters of the Moon)
+			[118374] = "Stage 3", -- Captain Yathae Moonstrike (Sisters of the Moon)
+		},
 	},
 	["SPELL_AURA_APPLIED"] = {
-		[244834] = {[117269] = "Intermission 1"} -- Nether Gale / Kil'jaeden
+		[244834] = { -- Nether Gale
+			[117269] = "Intermission 1", -- Kil'jaeden
+		},
 	},
 	["SPELL_AURA_REMOVED"] = {
-		[244834] = {[117269] = "Stage 2"} -- Nether Gale / Kil'jaeden
+		[244834] = { -- Nether Gale
+			[117269] = "Stage 2", -- Kil'jaeden
+		},
 	},
 	["SPELL_CAST_START"] = {
-		[241983] = {[117269] = "Intermission 2"} -- Deceiver's Veil / Kil'jaeden
+		[241983] = { -- Deceiver's Veil
+			[117269] = "Intermission 2", -- Kil'jaeden
+		},
 	},
 	["SPELL_AURA_REMOVED"] = {
-		[241983] = {[117269] = "Stage 3"} -- Deceiver's Veil / Kil'jaeden
+		[234891] = { -- Wrath of the Creators
+			[118289] = "Wrath Over", -- Maiden of Vigilance
+		},
+		[241983] = { -- Deceiver's Veil
+			[117269] = "Stage 3", -- Kil'jaeden
+		},
 	},
 }
