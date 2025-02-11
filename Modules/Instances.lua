aba8ab265b4780f45b29589e3333944edd109afa
funkydude <funkydude@users.noreply.github.com>
2025-02-11 01:50:58 +0000

    Add more instances

--- Modules/Instances.lua
+++ Modules/Instances.lua
@@ -5,6 +5,7 @@ do
 end
 
 addonTbl.INSTANCES = {
+	-- Dungeons
 	[2648] = true, -- The Rookery
 	[2649] = true, -- Priory of the Sacred Flame
 	[2651] = true, -- Darkflame Cleft
@@ -13,7 +14,14 @@ addonTbl.INSTANCES = {
 	[2661] = true, -- Cinderbrew Meadery
 	[2662] = true, -- The Dawnbreaker
 	[2669] = true, -- City of Threads
+	[2773] = true, -- Operation: Floodgate
 
+	-- Season 2 Dungeons
+	[1594] = true, -- The Motherlode!!
+	[2097] = true, -- Operation: Mechagon
+	[2293] = true, -- Theater of Pain
+
+	-- Raids
 	[2657] = true, -- Nerub-ar Palace
 	[2769] = true, -- Undermine
 }
--- Modules/Instances_Vanilla.lua
+++ Modules/Instances_Vanilla.lua
@@ -5,6 +5,7 @@ do
 end
 
 addonTbl.INSTANCES = {
+	-- Dungeons
 	[33] = true, -- Shadowfang Keep
 	[34] = true, -- The Stockade
 	[36] = true, -- The Deadmines
@@ -14,6 +15,22 @@ addonTbl.INSTANCES = {
 	[229] = true, -- Blackrock Spire
 	[329] = true, -- Stratholme
 	[429] = true, -- Dire Maul
-	[2784] = true, -- Demon Fall Canyon
-	[2875] = true, -- Karazhan Crypts
+	[2784] = true, -- Demon Fall Canyon [Classic Season of Discovery Only]
+	[2875] = true, -- Karazhan Crypts [Classic Season of Discovery Only]
+
+	-- Raids
+	[249] = true, -- Onyxia's Lair
+	[409] = true, -- Molten Core
+	[469] = true, -- Blackwing Lair
+	[309] = true, -- Zul'Gurub
+	[509] = true, -- Ruins of Ahn'Qiraj
+	[531] = true, -- Ahn'Qiraj Temple
+	[533] = true, -- Naxxramas
+	--[48] = true, -- Blackfathom Deeps [Classic Season of Discovery Only]
+	--[90] = true, -- Gnomeregan [Classic Season of Discovery Only]
+	--[109] = true, -- Sunken Temple [Classic Season of Discovery Only]
+	[2789] = true, -- The Tainted Scar (Lord Kazzak) [Classic Season of Discovery Only]
+	[2791] = true, -- Storm Cliffs (Azuregos) [Classic Season of Discovery Only]
+	[2804] = true, -- The Crystal Vale (Thunderaan) [Classic Season of Discovery Only]
+	[2832] = true, -- Nightmare Grove (Emeriss/Lethon/Taerar/Ysondre) [Classic Season of Discovery Only]
 }
