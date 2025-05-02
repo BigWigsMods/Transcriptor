ea14c6eed115722a08e5fd362d9ff7ad593dcc62
funkydude <funkydude@users.noreply.github.com>
2025-05-02 17:32:52 +0100

    Add instances list for Mists

---
+++ Modules/Instances_Mists.lua
@@ -0,0 +1,26 @@
+local addonTbl
+do
+	local _
+	_, addonTbl = ...
+end
+
+addonTbl.INSTANCES = {
+	-- Dungeons
+	[959] = true, -- Shado-Pan Monastery
+	[960] = true, -- Temple of the Jade Serpent
+	[961] = true, -- Stormstout Brewery
+	[962] = true, -- Gate of the Setting Sun
+	[994] = true, -- Mogu'shan Palace
+	[1001] = true, -- Scarlet Halls
+	[1007] = true, -- Scholomance
+	[1011] = true, -- Siege of Niuzao Temple
+	[1112] = true, -- Pursuing the Black Harvest
+	[1004] = true, -- Scarlet Monastery
+
+	-- Raids
+	[1009] = true, -- Heart of Fear
+	[996] = true, -- Terrace of Endless Spring
+	[1008] = true, -- Mogu'shan Vaults
+	[1098] = true, -- Throne of Thunder
+	[1136] = true, -- Siege of Orgrimmar
+}
