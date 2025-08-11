BigWigsMods/Transcriptor

commit 7197f26939a30b6cc71d72ec5139888cf3e4e8fb

Fix error on right-click, closes #47

.luacheckrc
@@ -53,7 +53,6 @@ read_globals = {
 	"GetSubZoneText",
 	"GetTime",
 	"GetZoneText",
-	"IsAltKeyDown",
 	"InCombatLockdown",
 	"IsEncounterInProgress",
 	"IsEncounterLimitingResurrections",

Transcriptor.lua
@@ -55,7 +55,7 @@ local UnitInRaid, UnitInParty, UnitIsFriend, UnitCastingInfo, UnitChannelInfo = 
 local UnitCanAttack, UnitExists, UnitIsVisible, UnitGUID, UnitClassification = UnitCanAttack, UnitExists, UnitIsVisible, UnitGUID, UnitClassification
 local UnitName, UnitPower, UnitPowerMax, UnitPowerType, UnitHealth = UnitName, UnitPower, UnitPowerMax, UnitPowerType, UnitHealth
 local UnitLevel, UnitCreatureType, UnitPercentHealthFromGUID, UnitTokenFromGUID = UnitLevel, UnitCreatureType, UnitPercentHealthFromGUID, UnitTokenFromGUID
-local GetInstanceInfo, IsAltKeyDown = GetInstanceInfo, IsAltKeyDown
+local GetInstanceInfo = GetInstanceInfo
 local GetZoneText, GetRealZoneText, GetSubZoneText = GetZoneText, GetRealZoneText, GetSubZoneText
 local GetSpellName = C_Spell and C_Spell.GetSpellName or GetSpellInfo
 local GetBestMapForUnit = C_Map.GetBestMapForUnit
@@ -1683,10 +1683,10 @@ end
 --
 
 local menu = {}
-local popupFrame = CreateFrame("Frame", "TranscriptorMenu", eventFrame, "UIDropDownMenuTemplate")
-local function openMenu(frame)
-	EasyMenu(menu, popupFrame, frame, 20, 4, "MENU")
-end
+--local popupFrame = CreateFrame("Frame", "TranscriptorMenu", eventFrame, "UIDropDownMenuTemplate")
+--local function openMenu(frame)
+--	EasyMenu(menu, popupFrame, frame, 20, 4, "MENU")
+--end
 
 local ldb = LibStub:GetLibrary("LibDataBroker-1.1"):NewDataObject("Transcriptor", {
 	type = "data source",
@@ -1708,10 +1708,8 @@ local ldb = LibStub:GetLibrary("LibDataBroker-1.1"):NewDataObject("Transcriptor"
 			else
 				Transcriptor:StopLog()
 			end
-		elseif button == "RightButton" then
-			openMenu(self)
-		elseif button == "MiddleButton" and IsAltKeyDown() then
-			Transcriptor:ClearAll()
+		--elseif button == "RightButton" then
+			--openMenu(self) -- XXX make a new menu?
 		end
 	end,
 })

commit 804c83bba026b7e9132c56c0ced253720ce7c06d

Fix alt middle click not wiping logs, regression from 7197f26939a30b6cc71d72ec5139888cf3e4e8fb, closes #52

.luacheckrc
@@ -54,6 +54,7 @@ read_globals = {
 	"GetTime",
 	"GetZoneText",
 	"InCombatLockdown",
+	"IsAltKeyDown",
 	"IsEncounterInProgress",
 	"IsEncounterLimitingResurrections",
 	"IsEncounterSuppressingRelease",

Transcriptor.lua
@@ -1776,6 +1776,8 @@ local ldb = LibStub:GetLibrary("LibDataBroker-1.1"):NewDataObject("Transcriptor"
 			end
 		elseif button == "RightButton" then
 			MenuUtil.CreateContextMenu(self, setupMenu)
+		elseif button == "MiddleButton" and IsAltKeyDown() then
+			Transcriptor:ClearAll()
 		end
 	end,
 })